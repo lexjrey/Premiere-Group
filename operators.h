1f54fb113b2dfff46d154b91a567abd4b2610b60
Tyler <lexjreyty@gmail.com>
2017-05-11 14:59:27 -0700

    Create operators.h

---
+++ operators.h
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int add(int x, int y){
+    return x + y;
+}
+
+int subtract(int x, int y){
+    return x - y;
+}
+
+int multiply(int x, int y){
+    return x * y;
+}
+
+int divide(int x, int y){
+    return x/y;
+}
+
+int exp(int x, int y){
+    return pow(x,y);
+}
+
+int mod(int x, int y){
+    return x % y;
+}
